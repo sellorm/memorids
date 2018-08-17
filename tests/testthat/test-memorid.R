95b4a09c238f10794668a761ab4c913b1fedc481
Mark Sellors <github@5vcc.com>
2018-08-17 22:35:55 +0100

    fixed regex

--- tests/testthat/test-memorid.R
+++ tests/testthat/test-memorid.R
@@ -1,11 +1,11 @@
 context("output checks")
 
 test_that("output is the correct format", {
-  expect_match(memorids::memorid(), "[a-z]*-[a-z]*")
+  expect_match(memorids::memorid(), "^[a-z]*-[a-z]*$")
 })
 
 test_that("sep produces the correct output", {
-  expect_match(memorids::memorid(sep = "+"), "[a-z]*\\+[a-z]*")
+  expect_match(memorids::memorid(sep = "+"), "^[a-z]*\\+[a-z]*$")
 })
 
 test_that("output is a single string", {
