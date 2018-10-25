fdb5eb1882dbc10ca9c87e4e21d620f9b6f283bb
Mark Sellors <github@5vcc.com>
2018-10-26 00:23:02 +0100

    Changed the tests to use full reference for the data objects.
    Tests are failing on travis-ci due to the objects not being available.
    
    Using the full ':::' reference for the objects will hopefully resolve this issue.

--- tests/testthat/test-words.R
+++ tests/testthat/test-words.R
@@ -1,16 +1,16 @@
 context("adjectives and nouns")
 
 test_that("words only have lower case characters", {
-  expect_match(adjectives, "^[a-z]*$")
-  expect_match(nouns, "^[a-z]*$")
+  expect_match(memorids:::adjectives, "^[a-z]*$")
+  expect_match(memorids:::nouns, "^[a-z]*$")
 })
 
 test_that("Both lists are character vectors", {
-  expect_is(adjectives, "character")
-  expect_is(nouns, "character")
+  expect_is(memorids:::adjectives, "character")
+  expect_is(memorids:::nouns, "character")
 })
 
 test_that("there are no duplicates", {
-  expect_false(unique(duplicated(adjectives)))
-  expect_false(unique(duplicated(nouns)))
+  expect_false(unique(duplicated(memorids:::adjectives)))
+  expect_false(unique(duplicated(memorids:::nouns)))
 })
