66c419bb8bf1b398a5dd3eb668cc2b42b298415c
Mark Sellors <github@5vcc.com>
2018-11-09 00:02:11 +0000

    Simplified the hook

--- pre-commit.R
+++ pre-commit.R
@@ -6,19 +6,8 @@ nounfile <- "data-raw/nouns.txt"
 adjfile <- "data-raw/adjectives.txt"
 sysdatafile <- "R/sysdata.rda"
 
-error_flag <- FALSE
 
-if ( file.mtime(nounfile) > file.mtime(sysdatafile)){
-  cat("Error:", nounfile, "is newer than", sysdatafile, "\n")
-  error_flag <- TRUE
-}
-
-if ( file.mtime(adjfile) > file.mtime(sysdatafile)){
-  cat("Error:", adjfile, "is newer than", sysdatafile, "\n")
-  error_flag <- TRUE
-}
-
-if ( error_flag ){
-  cat("Run 'make' in the ./raw-data\n")
-  quit(save = "no", status = 1, runLast = FALSE)
+if ( file.mtime(nounfile) > file.mtime(sysdatafile) |
+  file.mtime(adjfile) > file.mtime(sysdatafile) ){
+  stop("Source files newer than target\n  Run 'make' in ./data-raw")
 }
